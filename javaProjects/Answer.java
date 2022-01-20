2e7f7010029639f112adf582b046b45fe641f2ed
rishket <66543602+rishket@users.noreply.github.com>
2022-01-19 22:37:26 -0700

    Update Answer.java

--- javaProjects/Answer.java
+++ javaProjects/Answer.java
@@ -1,18 +1,9 @@
-// Assignment #: 8
-// Name: Your name
-// StudentID: your id
-// Lecture: your lecture
-// Section: your section
-// Description: This class contains a 2-dimensional array to
-// display the content of the checker board.
-
 import java.util.Stack;
 
 public class Answer
  {
    private char[][] solution;
 
-   //Constructor to create a 2-dimensional array and initialize it.
    public Answer(int size)
     {
      solution = new char[size][size];
@@ -22,15 +13,11 @@ public class Answer
         solution[i][j] = '_';
      }
 
-  //This method changes the element at row and col to have 'Q'
-  //to say that there is a queen placed at the location.
   public void setQueen(int row, int col)
    {
      solution[row][col] = 'Q';
    }
 
-  //The copySolution method copies the location of queens from
-  //the parameter stack into the 2-dimensional array
   public void copySolution(Stack solution)
    {
      while (!solution.isEmpty())
@@ -39,21 +26,15 @@ public class Answer
         setQueen(current.getRow(), current.getColumn());
        }
    }
-
-  //The toString method returns a string to display the content of
-  //the n queen problem solution
   public String toString()
    {
     String result = "";
-
     result += " |";
-
      for (int j = 0; j < solution.length; j++)
       {
        result += j + "|";
       }
      result += "\n";
-
      for (int i = 0; i < solution.length; i++)
       {
         result += i + "|";
@@ -63,9 +44,6 @@ public class Answer
         }
        result += "\n";
       }
-
      return result;
    }
-
 }//end of the Answer class
-
